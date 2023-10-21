8705145a4f91994852c6299a230fa2189bd92a65
kleuvyn <kleuvyn@hotmail.com>
2023-10-20 23:34:37 -0300

    fazendo submenu

--- Front/script.js
+++ Front/script.js
@@ -3,12 +3,36 @@ function toggleMenu() {
     menuOptions.classList.toggle("show-options");
 }
 
-function selecionarOpcao(opcao) {
+function toggleOpcoesCadastro() {
+    var menuOptions = document.getElementById("cadastro-options");
+    menuOptions.classList.toggle("opcoes-cadastro_exibir");
+}
+function toggleOpcoesVendas() {
+    var menuOptions = document.getElementById("vendas-options");
+    menuOptions.classList.toggle("elemento-escondido");
+}
+function toggleOpcoesMenuGerencial() {
+    var menuOptions = document.getElementById("menuGrerencial-options");
+    menuOptions.classList.toggle("opcoes-menuGrerencial_exibir");
+}
+function toggleOpcoesMenuFiscal() {
+    var menuOptions = document.getElementById("menuFiscal-options");
+    menuOptions.classList.toggle("opcoes-menuFiscal_exibir");
+}
+function toggleOpcoesEstoque() {
+    var menuOptions = document.getElementById("estoque-options");
+    menuOptions.classList.toggle("opcoes-estoque_exibir");
+}
+function toggleOpcoesFinanceiro() {
+    var menuOptions = document.getElementById("financeiro-options");
+    menuOptions.classList.toggle("opcoes-financeiro_exibir");
+}
 
-document.getElementById("sim").classList.remove("selecionado");
-document.getElementById("nao").classList.remove("selecionado");
-document.getElementById(opcao).classList.add("selecionado");
-document.getElementById("opcao").value = opcao;
+function selecionarOpcao(opcao) {
+    document.getElementById("sim").classList.remove("selecionado");
+    document.getElementById("nao").classList.remove("selecionado");
+    document.getElementById(opcao).classList.add("selecionado");
+    document.getElementById("opcao").value = opcao;
 }
 
 function selecionarGenero(genero) {
@@ -18,3 +42,11 @@ function selecionarGenero(genero) {
     document.getElementById(genero).classList.add("selecionado");
     document.getElementById("genero").value = genero;
 }
+
+// Adicionar funcionalidade para mostrar/ocultar o submenu
+document.querySelector('li:has(ul)').addEventListener('click', function() {
+    var submenu = this.querySelector('.submenu');
+    if (submenu) {
+        submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+    }
+});
