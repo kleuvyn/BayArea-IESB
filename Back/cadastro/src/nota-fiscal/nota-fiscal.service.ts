b8b789c4bfa984397bacb1e8d8e8e26e8c21793e
kleuvyn <kleuvyn@hotmail.com>
2023-10-27 21:47:41 -0300

    validando

--- Back/cadastro/src/nota-fiscal/nota-fiscal.service.ts
+++ Back/cadastro/src/nota-fiscal/nota-fiscal.service.ts
@@ -1,38 +1,75 @@
 import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { ClienteService } from 'src/cliente/cliente.service';
 
 export class NotaFiscalService {
-  constructor() {}
+  private prisma: PrismaClient;
+  private clienteService: ClienteService;
+
+
+
+  constructor(prisma: PrismaClient, clienteService: ClienteService) {
+    this.prisma = prisma;
+    this.clienteService = clienteService;
+  }
+
+
 
   async findAllNotasFiscais() {
-    return prisma.notaFiscal.findMany();
+    return this.prisma.notaFiscal.findMany();
   }
+
+
 
   async findNotaFiscalById(id: number) {
-    return prisma.notaFiscal.findUnique({
+    if (id === null) {
+      throw new Error('ID inválido!');
+    }
+  
+    return this.prisma.notaFiscal.findUnique({
       where: { id },
     });
   }
+  
+
 
   async createNotaFiscal(data: any) {
-    data.dataEmissao = new Date(2016, 2, 3).toISOString();
-
-    return prisma.notaFiscal.create({
+    if (!data.dataEmissao || isNaN(new Date(data.dataEmissao).getTime())) {
+      throw new Error('Data de emissão inválida!');
+    }
+    return this.prisma.notaFiscal.create({
       data,
     });
   }
 
-  async updateNotaFiscal(id: number, data: any) {
-    return prisma.notaFiscal.update({
-      where: { id },
-      data,
-    });
-  }
 
-  async deleteNotaFiscal(id: number) {
-    return prisma.notaFiscal.delete({
-      where: { id },
-    });
+ async updateNotaFiscal(id: number, data: any) {
+  if (id === null || typeof id !== 'number') {
+    throw new Error('ID inválido!');
   }
+  if (!data.dataEmissao || isNaN(new Date(data.dataEmissao).getTime())) {
+    throw new Error('Data de emissão inválida!');
+  }
+  return this.prisma.notaFiscal.update({
+    where: { id },
+    data,
+  });
+}
+
+
+
+async deleteNotaFiscal(id: number) {
+  if (id === null || typeof id !== 'number') {
+    throw new Error('ID inválido!');
+  }
+  const existingNotaFiscal = await this.prisma.notaFiscal.findUnique({
+    where: { id },
+  });
+
+  if (!existingNotaFiscal) {
+    throw new Error('Nota fiscal não encontrada.');
+  }
+  return this.prisma.notaFiscal.delete({
+    where: { id },
+  });
+}
 }
